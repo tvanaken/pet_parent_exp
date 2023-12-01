c272a6b1464da9e644f8e558f51b8e5fcb67886a
Sarah <vanwars@gmail.com>
2023-11-30 20:44:26 -0500

    DB fixed?

--- src/db/db_setup.sh
+++ src/db/db_setup.sh
@@ -6,12 +6,13 @@ echo "$(ls -la)"
 pg_pass=$(cat ./.env | grep PGPASSWORD | awk -F= '{print $2}')
 DB_USERNAME=$(cat ./.env | grep DB_USERNAME | awk -F= '{print $2}')
 DB_PASSWORD=$(cat ./.env | grep DB_PASSWORD | awk -F= '{print $2}')
+DB_HOST=$(cat ./.env | grep DB_HOST | awk -F= '{print $2}')
+DB_PORT=$(cat ./.env | grep DB_PORT | awk -F= '{print $2}')
+
 # Set the postgres password environment variable
-export PGPASSWORD="postgres"
+export PGPASSWORD=$pg_pass
+
 # Create new DB user with appropriate permissions:
-# psql -U postgres -h localhost -p 5432 -c "CREATE USER "$DB_USERNAME" with encrypted password '"$DB_PASSWORD"';"
-# psql -U postgres -h localhost -p 5432 -c "GRANT USAGE ON SCHEMA public TO "$DB_USERNAME";"
-# psql -U postgres -h localhost -p 5432 -c "ALTER USER "$DB_USERNAME" CREATEDB;"
-psql -U postgres -h localhost -p 5432 -c "CREATE USER app_user with encrypted password '12345';"
-psql -U postgres -h localhost -p 5432 -c "GRANT USAGE ON SCHEMA public TO app_user;"
-psql -U postgres -h localhost -p 5432 -c "ALTER USER app_user CREATEDB;"
+psql -U postgres -h $DB_HOST -p $DB_PORT -c "CREATE USER "$DB_USERNAME" with encrypted password '"$DB_PASSWORD"';"
+psql -U postgres -h $DB_HOST -p $DB_PORT -c "GRANT USAGE ON SCHEMA public TO "$DB_USERNAME";"
+psql -U postgres -h $DB_HOST -p $DB_PORT -c "ALTER USER "$DB_USERNAME" CREATEDB;"
